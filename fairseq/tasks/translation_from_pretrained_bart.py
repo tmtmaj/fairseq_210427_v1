02e09cb7c889fbf7817233aaae3548d5ab0d0009
박정혁[MT Science] <jeonghyeok.park@navercorp.com>
2021-05-31 20:46:59 +0900

    Update translation_from_pretrained_bart.py

--- fairseq/tasks/translation_from_pretrained_bart.py
+++ fairseq/tasks/translation_from_pretrained_bart.py
@@ -52,6 +52,7 @@ class TranslationFromPretrainedBARTTask(TranslationTask):
     def __init__(self, args, src_dict, tgt_dict):
         super().__init__(args, src_dict, tgt_dict)
         self.langs = args.langs.split(",")
+        self.args = args
         for d in [src_dict, tgt_dict]:
             for l in self.langs:
                 d.add_symbol("[{}]".format(l))
